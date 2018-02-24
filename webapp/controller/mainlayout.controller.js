156536edcbc52d99893f57430df0191b3bd59301
Alessandro Mainas <bortoman0@gmail.com>
2018-02-24 09:40:57 +0000

    Top bar menu

--- webapp/controller/mainlayout.controller.js
+++ webapp/controller/mainlayout.controller.js
@@ -1,9 +1,13 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function(Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("progresstoolsTeam4progresstools.controller.mainlayout", {
-
+		onUserNamePress: function(event) {
+			sap.m.MessageToast.show("This message should appear in the message toast", {
+			});
+		}
 	});
 });
